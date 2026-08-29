45e827feac3041e2d53cb6a447c0d534e6d16b1e
renatovalerio88 <valerio.renato@hotmail.com>
2026-08-29 15:59:06 -0300

    fix: calibrar PP em -4 pela auditoria empirica de 2026

--- src/cartola_scoring.py
+++ src/cartola_scoring.py
@@ -11,7 +11,7 @@ SCOUT_WEIGHTS: dict[str, float] = {
     "G": 8.0, "A": 5.0, "FT": 3.0, "FD": 1.2, "FF": 0.8, "FS": 0.5,
     "PS": 1.0, "I": -0.1, "DS": 1.5, "SG": 5.0, "DP": 7.0, "DE": 1.3,
     "GC": -3.0, "CV": -3.0, "CA": -1.0, "GS": -1.0, "FC": -0.3,
-    "PC": -1.0, "PP": -3.2,
+    "PC": -1.0, "PP": -4.0,
 }
 POSITION_NAMES = {1: "GOL", 2: "LAT", 3: "ZAG", 4: "MEI", 5: "ATA", 6: "TEC"}
 COUNT_SCOUTS = set(SCOUT_WEIGHTS)
